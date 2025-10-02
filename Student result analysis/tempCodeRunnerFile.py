b12fd8337b4e317ce21079bbf80ae094ee0effc6
Vansh1621 <ajex732006@gmail.com>
2025-10-02 19:11:27 +0530

    Delete Student result analysis directory

--- Student result analysis/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-subjects = ["Maths", "Physics", "Chemistry", "English", "Computer"]
-for sub in subjects:
-    topper = df.loc[df[sub].idxmax()]
-    print(f"{sub}: {topper['Name']} (Marks: {topper[sub]})")
-
-exam_topper = df.loc[df["Total"].idxmax()]
-print(f"Name: {exam_topper['Name']}, Total: {exam_topper['Total']}, Avg: {exam_topper['Average']}")
